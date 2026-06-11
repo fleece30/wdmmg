c382c5d6be5da1a7c5ded2471f48ad4f08bfafa5
fleece30 <abhiyad25@gmail.com>
2026-06-10 22:46:37 -0700

    chore: refactored and added basic components (#1)

---
+++ ui/components/AppSidebar.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link"
+import Image from "next/image"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar"
+
+const AppSidebar = () => {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <Image
+                  src="https://4kwallpapers.com/images/walls/thumbs_2t/18166.jpg"
+                  alt="logo"
+                  width={20}
+                  height={20}
+                />
+                <span>Lama Dev</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
+
+export default AppSidebar
--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   // output: "standalone",
-};
+  images: {
+    remotePatterns: [new URL("https://4kwallpapers.com/**")],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
